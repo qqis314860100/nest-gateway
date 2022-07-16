9014a429fb90a0093cbe402f76d91cbb7dc6a679
ban9ban9da@qq.com <ban9ban9da@qq.com>
2022-07-16 18:47:04 +0800

    feat: 安装swagger文档

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/exceptions/base.exception.filter';
 import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { generateDocument } from './doc';
 
 declare const module: any;
 
@@ -27,6 +28,8 @@ async function bootstrap() {
     type: VersioningType.URI,
   });
 
+  // Swagger 会默认收集我们的接口信息
+  generateDocument(app);
   // 全局拦截器，捕获返回统一格式
   app.useGlobalInterceptors(new TransformInterceptor());
   // 全局异常过滤器
