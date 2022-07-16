03a32408f21cb5add635a2aa37df4a09c02566c8
ban9ban9da@qq.com <ban9ban9da@qq.com>
2022-07-16 18:31:36 +0800

    feat: 热重载

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import { VersioningType, VERSION_NEUTRAL } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+import { AllExceptionFilter } from './common/exceptions/base.exception.filter';
+import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+
+declare const module: any;
+
+async function bootstrap() {
+  console.log('module: ' + module);
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  // 启用版本控制
+  app.enableVersioning({
+    defaultVersion: [VERSION_NEUTRAL, '1', '2'],
+    type: VersioningType.URI,
+  });
+
+  // 全局拦截器，捕获返回统一格式
+  app.useGlobalInterceptors(new TransformInterceptor());
+  // 全局异常过滤器
+  app.useGlobalFilters(new AllExceptionFilter(), new HttpExceptionFilter());
+  await app.listen(3000);
+}
+bootstrap();
